0fe89f88b0fd99400c2b7e745f458cb246033058
LiberSpiritus <kunio1881@naver.com>
2020-11-27 20:02:18 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// 위에 있는 navbar를 투명하게 만들기 
+// 1. 위에 있는 navbar를 투명하게 만들기 
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
@@ -14,4 +14,28 @@ document.addEventListener('scroll', () => {
     }
 
 });
+
+//2. scroll to section
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click',(e) => {
+    // html element 속성 값중 data- 뒤에있는 속성의 값을 가져오기위해
+    //dataset.(data-뒤에있는 속성이름)으로 가져온다.
+    
+    const link = e.target.dataset.aaaa;
+    if(link === undefined){
+        return;
+    }
+
+    console.log(link);
+
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+
+
+});
+// navbarMenu.addEventListener('click',function (){
+//     console.log('asdasdas');
+//     console.log(this.className);
+// });
 
